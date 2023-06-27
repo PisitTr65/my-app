21914b210c694c09575c7066f6d47e9e51f7d9c7
PisitTr65 <pisit.tr.65@ubu.ac.th>
2023-06-27 14:29:15 +0700

    app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
+import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 import Footer from './Footer';
 import { useState } from 'react';
 
 function App() {
-  const [num, setNum] = useState(0);  
-  return (
-    <div className="App">
+  const [num, setNum] = useState(0);
+    let isPrime = '';
+    if (num === 1){
+      isPrime = (`${num} เป็นทั้งจำนวนเฉพาะและ<strong>ไม่</strong>เป็นจำนวนเฉพาะ`)
+    }
+    else if (num > 1){
+      for (let i = 2; i < num; i++){
+        if(num % i === 0){
+          isPrime = (`${num} เป็นจำนวนเฉพาะ`);
+        }
+      }
+      if (isPrime){
+        isPrime = (`${num}<strong> ไม่</strong>เป็นจำนวนเฉพาะ`)
+      }else{
+        isPrime = (`${num} เป็นจำนวนเฉพาะ`);
+      }
+    }
+  return(
+    <div className='App'>
       <Header />
-      <div className="MyContent">
+      <div className='MyContent'>
         <h2>{num}</h2>
-        <button onClick={()=>setNum(num+1)}>เพิ่ม</button>
-        <button onClick={()=>setNum(num-1)}>ลด</button>
+        <button onClick={()=>setNum(num + 1)} class="btn btn-primary">เพิ่ม</button>
+        &nbsp;
+        <button onClick={()=>setNum(num - 1)} class="btn btn-secondary">ลด</button>
+        <br></br>
+        <span dangerouslySetInnerHTML={{__html: isPrime}}></span> 
       </div>
       <Footer />
     </div>
